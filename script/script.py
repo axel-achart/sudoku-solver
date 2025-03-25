5b003633f287d0d4b6d3baffd46b255030ebd053
axel-achart <axel.achart@gmail.com>
2025-03-25 09:35:34 +0100

    init class sudokugrid

--- script/script.py
+++ script/script.py
@@ -1,6 +1,9 @@
-# A .py script containing a SudokuGrid class whose methods allow you to:
+# A  script.py containing a SudokuGrid class whose methods allow you to:
 # a. Import and parse a Sudoku grid,
 # b. Display the grid elements,
 # c. Solve using brute force and backtracking
 # d. A file that retrieves a Sudoku example, calls one
 # of the solving methods, and displays the solution in the terminal as well as in a Pygame window.
+
+class SudokuGrid:
+    pass
