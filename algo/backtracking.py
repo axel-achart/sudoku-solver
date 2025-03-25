e9a553d8aa2ab7244c5ca6242304c85dd42e2e43
alexandre <alexandre.chevalier@laplateforme.io>
2025-03-25 23:23:27 +0100

    the code solved a sudoku grid now i try to implented with the txt file

--- algo/backtracking.py
+++ algo/backtracking.py
@@ -12,19 +12,32 @@ grids = [
     [1,0,0,8,5,0,0,0,0]
 ]
 
-### True si valeur non trouve
-### False si valeur trouvée
+### True if we didn't find the value
+### False if i find value
 
 class Backtracking():
     def __init__(self, grid):
         self.grid = grid
 
+    def display_grid(self):
+        for i in range(9):
+            if i % 3 == 0 and i != 0:
+                print("-" * 21)
+            for j in range(9):
+                if j % 3 == 0 and j != 0:
+                    print("|", end=" ")
+                print(self.grid[i][j] if self.grid[i][j] != 0 else ".", end=" ")
+            print()
+        print("\n")
+   
+## methods who verified if the number is in the line
     def line_value(self, value,line):
         for i in range(0,9):
             if self.grid[line][i] == value:
                 return False
         return True
-                
+
+## methods who verified if the number is in the column           
     def column_value(self, value, column):
         for i in range(0,9):
             if self.grid[i][column] == value:
@@ -32,16 +45,37 @@ class Backtracking():
         return True
     
     def box_grid_value(self, value, line, column):
-        _line = line - (line%3)
-        _column = column - (column%3)
 
-        for i in range(_line, _column):
-            for j in range(_line, _column): 
+        _line = line - (line % 3)  
+        _column = column - (column % 3)
+
+        for i in range(_line, _line + 3):
+            for j in range(_column, _column + 3): 
                 if self.grid[i][j] == value:
                     return False
         return True
         
-    
+    def verification_grid(self, position=0):
+        if position == 81:
+            return True
+
+        row, col = divmod(position, 9)
+
+        if self.grid[row][col] != 0:
+            return self.verification_grid(position + 1)
+
+        for value in range(1, 10):
+            print(self.display_grid())
+            if (self.line_value(value, row) and
+                self.column_value(value, col) and
+                self.box_grid_value(value, row, col)):
+
+                self.grid[row][col] = value
+                if self.verification_grid(position + 1):
+                    return True
+                self.grid[row][col] = 0
+
+        return False
             
 
 
@@ -50,7 +84,13 @@ grid = Backtracking(grids)
 tab1 = grid.line_value(1, 0)
 tab2 = grid.column_value(1, 0)
 tab3 = grid.box_grid_value(1,2,3)
-print(tab3)
+grid.display_grid()
+if grid.verification_grid(0):
+    print('the sudoku is solved')
+    grid.display_grid()
+else:
+    print("No solution found.")
 
 
-##grid.column_value(2)
+
+
